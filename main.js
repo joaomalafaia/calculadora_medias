b5c5ffafffa47bc81a257007ba637747d1cfec8a
João Malafaia <j.malafaia02@gmail.com>
2023-06-27 16:47:27 -0300

    comentários adicionados no js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji triste"/>'
 const atividades = []; // precisamos das arrays para calcular a média final, porque são valores separados em cada registro
 const notas = []; // definindo as constantes, notas e nomes de atividades são arrays. Começam como arrays vazios
 const spanAprovado = "<span class='resultado aprovado'>Aprovado!</span></td>"
-const spanReprovado = "<span class='resultado reprovado'>Reprovado!</span></td>"
+const spanReprovado = "<span class='resultado reprovado'>Reprovado!</span></td>" // spans (textos) que aparecem como as mensagens
 const notaMinima = parseFloat(prompt('Digitea nota mínima: '));
 
 let linhas = ''; // as linhas da tabela começam em branco
@@ -17,14 +17,14 @@ form.addEventListener('submit', function(e){ // essa função de evento chama to
 })
 
 function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNomeAtividade = document.getElementById('nome-atividade'); // estipulando as constantes
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
     if (atividades.includes(inputNomeAtividade.value)) {
-        alert (`A atividade: ${inputNomeAtividade.value} já foi inserida`);
+        alert (`A atividade: ${inputNomeAtividade.value} já foi inserida`); // esse if impede que coloquemos atividades com o mesmo nome, o .includes lê o que já foi colocado e impede isso
     } else{
         atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade.value));
+        notas.push(parseFloat(inputNotaAtividade.value)); // jogamos as arrays para o código
     
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -41,16 +41,16 @@ function adicionaLinha() {
 
 function atualizaTabela(){
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+    corpoTabela.innerHTML = linhas; // os dados são colocados na tabela
 }
 
 function calculaMediaFinal() {
-    let somaDasNotas = 0;
+    let somaDasNotas = 0; // soma começa em zero
 
-    for (let i = 0; i < notas.length; i++) {
+    for (let i = 0; i < notas.length; i++) { // PARA (for) cada vez que o i for menor que o número de notas - ou seja sempre - a soma das notas vai ser igual ao array de todas as notas juntas
         somaDasNotas += notas[i];
     }
-    return somaDasNotas / notas.length;
+    return somaDasNotas / notas.length; // o retorno dessa função é a soma das notas dividido pela quantidade de notas
 }
 
 function atualizaMediaFinal () {
